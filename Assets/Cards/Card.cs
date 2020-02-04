f7a147d9e262758bd90047bcd17bad0ff70a4697
Jeff <droodic.gamedev@gmail.com>
2020-02-04 15:23:35 -0500

    Card Generation

---
+++ Assets/Cards/Card.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Card")]
+public class Card : ScriptableObject
+{
+    public new string name;
+    public string desc;
+    public int mana;
+    public int attack;
+    public int hp;
+    public Sprite art;
+}
---
+++ Assets/Cards/CardDisplay.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+using TMPro;
+
+public class CardDisplay : MonoBehaviour, IPointerEnterHandler , IPointerExitHandler
+{
+    [SerializeField] Card card;
+    [SerializeField] Image cardArt;
+    [SerializeField] TextMeshProUGUI manaText;
+    [SerializeField] TextMeshProUGUI nameText;
+    [SerializeField] TextMeshProUGUI descText;
+    [SerializeField] TextMeshProUGUI attackText;
+    [SerializeField] TextMeshProUGUI hpText;
+    RectTransform rect;
+
+    void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+    }
+
+    void Start()
+    { 
+        cardArt.sprite = card.art;
+        manaText.text = card.mana.ToString();
+        attackText.text = card.attack.ToString();
+        hpText.text = card.hp.ToString();
+        nameText.text = card.name;
+        descText.text = card.desc;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        GetComponentInParent<GridLayoutGroup>().padding.top = 300;
+        LayoutRebuilder.MarkLayoutForRebuild(rect);
+        Debug.Log("Mouse over: " + card.name);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        GetComponentInParent<GridLayoutGroup>().padding.top = 675;
+        LayoutRebuilder.MarkLayoutForRebuild(rect);
+    }
+}
